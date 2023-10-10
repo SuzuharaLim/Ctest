dbe92a2a6d6d055f3634ca46a3d71802f67b8714
SuzuharaLim <suzuharalim@gmail.com>
2023-10-10 13:51:24 +0800

    up data

--- memcpy/memcpy.cpp
+++ memcpy/memcpy.cpp
@@ -30,27 +30,47 @@ int my_strlen(const char* str)
     return num;
 }
 
-char* my_strcat(char* dest,char* src) {
-    char* original_dest = dest;
-    original_dest = (char*)malloc(my_strlen(dest) * sizeof(*dest));//求字串長度
+char* my_strcat(char* dest, char* src) {
+    char* original_dest;
+     
+    int strAlen = strlen(dest);
+    int strBlen = strlen(src);
+    int len = strAlen + strBlen;
+    original_dest = (char*)malloc(strAlen + strAlen + 1);
+
+    char* retval = original_dest;
+
     while (*dest) {
+        *original_dest++ = *dest;
         dest++;
     }
-    for (int i = 0; i < sizeof(src); i++)
-        *dest++ = *src++;
-    return original_dest;
+    
+
+    int slen = strlen(src);
+
+    for (int i = 0; i < slen; i++)
+        *original_dest++ = *(src + i);
+   // while (*original_dest++ = *src++);
+
+    *original_dest = '\0';
+
+    return retval;
 }
 
 int main() {
-    char owo[7] = "oghjwo";
+    char owo[50] = "owo";
     char ouo[4] = "ouo";
     char test1[] = "test1";
     char test2[10] = "test2";
 
-    cout << strlen(test1) << endl;
-    cout << sizeof(test1) << ", " << sizeof(test2) << endl;
+    //cout << strlen(test1) << endl;
+    //cout << sizeof(test1) << ", " << sizeof(test2) << endl;
     //cout <<my_strcat(owo,ouo);
-    cout << sizeof(int*) << endl;
-    cout << my_strlen(owo);
+    //cout << sizeof(int*) << endl;
+    //cout << my_strlen(owo)<<endl<<endl;
+
+    cout << my_strcat(owo, ouo)<<endl;
+
+    cout << string(owo) + string(ouo) << endl;
     return 0;
 }
